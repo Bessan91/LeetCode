4f5a4e8e28b325315745682faf6d5937a448059c
BessanHussien91 <hussienbessan@gmail.com>
2025-04-15 00:05:30 +0300

    Add TwoSum.java inside arrays folder

---
+++ TwoSum_LeetCode/TwoSum.java
@@ -0,0 +1,31 @@
+/*
+ * Problem: Two Sum
+ * Source: LeetCode
+ * 
+ * Given an array of integers nums and an integer target, return indices of 
+ * the two numbers such that they add up to target.
+ */
+
+import java.util.HashMap;
+
+public class TwoSum {
+    public static int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int diff = target - nums[i];
+            if (map.containsKey(diff)) {
+                return new int[] { map.get(diff), i };
+            }
+            map.put(nums[i], i);
+        }
+        return new int[] {};
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {2, 7, 11, 15};
+        int target = 9;
+        int[] result = twoSum(nums, target);
+        System.out.println("Indices: [" + result[0] + ", " + result[1] + "]");
+    }
+}
+ 
